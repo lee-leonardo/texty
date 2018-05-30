add40363127a4a7f6e014a3002fde67accbd35cc
Leonardo Lee <lee.leonardo.king@gmail.com>
2018-05-29 21:24:21 -0700

    006 - redux, typo in TCSAFLUSH

--- c/kilo.c
+++ c/kilo.c
@@ -1,17 +1,20 @@
+#include <stdlib.h>
+
 #include <termios.h>
 #include <unistd.h>
 
 struct termios orig_termios;
 
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUCH, &orig_termios);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
 void enableRawMode() {
+  struct termios raw = orig_termios;
+
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
   raw.c_lflag &= ~(ECHO);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -23,7 +26,7 @@ int main() {
   enableRawMode();
 
   while(
-    read(STDIN_FILENO, &c, 1) == 1 
+    read(STDIN_FILENO, &c, 1) == 1
 	&& c != 'q'
   );
 
