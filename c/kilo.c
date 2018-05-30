46ebdb7bf97f00b77aa9867e7373c45f01ed017a
Leonardo Lee <lee.leonardo.king@gmail.com>
2018-05-29 21:31:39 -0700

    006 - redux, clang issues and also some restructuring so that it functions, was broken.

--- c/kilo.c
+++ c/kilo.c
@@ -9,10 +9,12 @@ void disableRawMode() {
 }
 
 void enableRawMode() {
+  struct termios raw;
+
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  raw = orig_termios;
   raw.c_lflag &= ~(ECHO);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
