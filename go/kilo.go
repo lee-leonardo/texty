2e6d5d91fc6ab59855dadd8b25842785192c8f5d
Leonardo Lee <lee.leonardo.king@gmail.com>
2018-06-03 21:08:05 -0700

    001 -> trying to start the code process.

--- go/kilo.go
+++ go/kilo.go
@@ -1,3 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const (
+	ESC   = 0
+	ENTER = 0
+	Q     = 0
+)
+
 func main() {
-  
+	var sig int
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Printf("Hello, World!")
+
+	for sig == 0 {
+		rune, _, err := reader.ReadRune()
+
+		if err != nil {
+			fmt.Print(err)
+			sig = -1
+		}
+
+		if rune == ENTER {
+			continue
+		}
+
+		if rune == ESC || rune == Q {
+			sig = 1
+			continue
+		}
+
+		fmt.Printf("%d %c", rune, rune)
+	}
 }
