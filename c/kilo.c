8b6338a56201bbd6d6c29351152ce631a8b30af4
Leonardo Lee <lee.leonardo.king@gmail.com>
2018-06-12 22:40:29 -0700

    011 adding to the c flags to read more control characters.

--- c/kilo.c
+++ c/kilo.c
@@ -18,7 +18,7 @@ void enableRawMode() {
 
   raw = orig_termios;
   raw.c_iflag &= ~(IXON);
-  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
