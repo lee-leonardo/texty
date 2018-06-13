3787ede09f71e1d04139e3167ff9cb9a157f9031
Leonardo Lee <lee.leonardo.king@gmail.com>
2018-06-12 22:43:42 -0700

    013 turn off output post processing output

--- c/kilo.c
+++ c/kilo.c
@@ -18,6 +18,7 @@ void enableRawMode() {
 
   raw = orig_termios;
   raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_oflag &= ~(OPOST);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
